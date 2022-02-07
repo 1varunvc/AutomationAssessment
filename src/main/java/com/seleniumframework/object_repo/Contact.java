9d5a9b4a0a0e61fea9956bcd6f5f0621d28952ee
Varun Chawla <varun.chawla@modmed.com>
2022-02-07 21:03:44 +0530

    Renames object_repo and adds locators.

--- src/main/java/com/seleniumframework/object_repo/Contact.java
+++ src/main/java/com/seleniumframework/object_repo/Contact.java
@@ -3,11 +3,11 @@ package com.seleniumframework.object_repo;
 import org.openqa.selenium.By;
 
 public class Contact {
-    public static final By INPUT_NAME = By.name("name");
-    public static final By INPUT_EMAIL = By.name("email");
-    public static final By INPUT_TELEPHONE = By.name("telephone");
-    public static final By INPUT_MESSAGE = By.name("message");
-    public static final By SUBMIT = By.linkText("Send message");
+    public static final By NAME_TXTBOX = By.name("name");
+    public static final By EMAIL_TXTBOX = By.name("email");
+    public static final By TELEPHONE_TXTBOX = By.name("telephone");
+    public static final By MESSAGE_TXTBOX = By.name("message");
+    public static final By SUBMIT_BTN = By.linkText("Send message");
     public static final By MESSAGE_SENT = By.xpath("//div[text()='Feedback has been sent to the administrator']");
     public static final By MESSAGE_INVALID_EMAIL = By.xpath("//div[text()='* Invalid email address']");
 }
